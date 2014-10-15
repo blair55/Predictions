6de1415cca59046b1c428be207dce58f345e00b0
Nick <blair.nicholas@googlemail.com>
2014-10-15 19:53:52 +0100

    created apphb hosting project and grunt dist target

--- src/Predictions.Host.AppHb/Properties/AssemblyInfo.cs
+++ src/Predictions.Host.AppHb/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Predictions.Host")]
+[assembly: AssemblyTitle("Predictions.Host.AppHb")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Predictions.Host")]
+[assembly: AssemblyProduct("Predictions.Host.AppHb")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
