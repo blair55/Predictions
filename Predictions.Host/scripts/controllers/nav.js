4f06fa36f7e093881754d241ad8f101ad5dab335
Nick <blair.nicholas@googlemail.com>
2014-10-21 21:52:46 +0100

    hacking navigation

--- Predictions.Host/scripts/controllers/nav.js
+++ Predictions.Host/scripts/controllers/nav.js
@@ -8,8 +8,17 @@
  * Controller of the frontendApp
  */
 angular.module('frontendApp')
-  .controller('NavCtrl', function ($scope, auth) {
+  .controller('NavCtrl', function ($scope, $location, auth) {
+      $scope.navCollapsed = true;
       auth.withPlayer(function (player) {
           $scope.player = player;
       });
+      $scope.navTo = function ($event) {
+          $scope.navCollapsed = true;
+          var i = $event.target.href.indexOf('#') + 1;
+          var path = $event.target.href.substring(i);
+          console.log(path);
+          $location.path(path);
+          $event.preventDefault();
+      }
   });
