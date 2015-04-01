f3261627a567e08ca289326a0046d37dfab4e9da
Nick <blair.nicholas@googlemail.com>
2015-04-01 01:08:00 +0100

    app keys moved to ignored config

--- Predictions.Api/Config.fs
+++ Predictions.Api/Config.fs
@@ -9,6 +9,7 @@ open Microsoft.AspNet.Identity
 open Microsoft.AspNet.Identity.Owin
 open System.Web.Http.Owin
 open System
+open System.Configuration
 open System.Net
 open System.Net.Http
 open System.Net.Http.Headers
@@ -16,8 +17,6 @@ open System.Web
 open System.Web.Http
 open System.Web.Routing
 open System.Web.Http.Filters
-
-//type HttpRoute = { controller:string; id:RouteParameter }
 
 type ErrorFilter() =
     inherit ExceptionFilterAttribute()
@@ -29,7 +28,7 @@ type ErrorFilter() =
 
 type Config() =
 
-    static member RegisterWebApi(config: HttpConfiguration) =
+    static member private RegisterWebApi(config: HttpConfiguration) =
         config.MapHttpAttributeRoutes()
         config.EnableCors()
         config.Filters.Add(new ErrorFilter())
@@ -39,8 +38,9 @@ type Config() =
 
     static member BuildApp(app:IAppBuilder) =
     
-        let userManager = new PlUserManager(new PlUserStore())
-        app.CreatePerOwinContext<PlSignInManager>(fun _ (c:IOwinContext) -> new PlSignInManager(userManager, c.Authentication)) |> ignore
+        let userManager() = new PlUserManager(new PlUserStore())
+        app.CreatePerOwinContext<PlSignInManager>(
+            fun _ (c:IOwinContext) -> new PlSignInManager(userManager(), c.Authentication)) |> ignore
         
         app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie)
 
@@ -49,13 +49,13 @@ type Config() =
         app.UseCookieAuthentication(cookieOptions) |> ignore
 
         let twitterOptions = new Twitter.TwitterAuthenticationOptions()
-        twitterOptions.ConsumerKey <- "zo4kCGVlAp2esQpqXfFvGqow1"
-        twitterOptions.ConsumerSecret <- "qSkztP6VB1a7jxVtLyHSVX13QqyzCGiT1zX5psvbco8UiTkohr"
+        twitterOptions.ConsumerKey <- "Cs5EW6KIh7gLlmmMfEPWj13uV"
+        twitterOptions.ConsumerSecret <- ConfigurationManager.AppSettings.["TwitterConsumerSecret"]
         app.UseTwitterAuthentication(twitterOptions) |> ignore
 
         let facebookOptions = new Facebook.FacebookAuthenticationOptions()
         facebookOptions.AppId <- "701632913289116"
-        facebookOptions.AppSecret <- "67e25b79fa689840e26520f20f620fa8"
+        facebookOptions.AppSecret <- ConfigurationManager.AppSettings.["FacebookAppSecret"]
         app.UseFacebookAuthentication(facebookOptions) |> ignore
 
         app.UseCors(CorsOptions.AllowAll) |> ignore
