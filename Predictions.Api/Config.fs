caf879d9c2a54c3c626b31e59e9f142176346cb8
nick <nick@adbrain.com>
2015-09-27 18:11:37 +0100

    fix twitter remote certificate validation error

--- Predictions.Api/Config.fs
+++ Predictions.Api/Config.fs
@@ -1,4 +1,4 @@
-namespace Predictions.Api
+﻿namespace Predictions.Api
 
 open Owin
 open Microsoft.Owin
@@ -77,9 +77,17 @@ type Config() =
         cookieOptions.AuthenticationType <- DefaultAuthenticationTypes.ApplicationCookie
         app.UseCookieAuthentication(cookieOptions) |> ignore
 
+        let ids = ["A5EF0B11CEC04103A34A659048B21CE0572D7D47"  // VeriSign Class 3 Secure Server CA - G2
+                   "0D445C165344C1827E1D20AB25F40163D8BE79A5"  // VeriSign Class 3 Secure Server CA - G3
+                   "7FD365A7C2DDECBBF03009F34339FA02AF333133"  // VeriSign Class 3 Public Primary Certification Authority - G5
+                   "39A55D933676616E73A761DFA16A7E59CDE66FAD"  // Symantec Class 3 Secure Server CA - G4
+                   "4eb6d578499b1ccf5f581ead56be3d9b6744a5e5"  // VeriSign Class 3 Primary CA - G5
+                   "5168FF90AF0207753CCCD9656462A212B859723B"  // DigiCert SHA2 High Assurance Server C‎A 
+                   "B13EC36903F8BF4701D498261A0802EF63642BC3"] // DigiCert High Assurance EV Root CA
         let twitterOptions = new Twitter.TwitterAuthenticationOptions()
         twitterOptions.ConsumerKey <- ConfigurationManager.AppSettings.["TwitterConsumerKey"]
         twitterOptions.ConsumerSecret <- ConfigurationManager.AppSettings.["TwitterConsumerSecret"]
+        twitterOptions.BackchannelCertificateValidator <- new CertificateSubjectKeyIdentifierValidator(ids)
         app.UseTwitterAuthentication(twitterOptions) |> ignore
 
         let facebookOptions = new Facebook.FacebookAuthenticationOptions()
