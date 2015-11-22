506e44381ba314cae4c6deab0be3eef95f3eb750
nick <nick@adbrain.com>
2015-11-22 17:25:57 +0000

    not comiling front end during build

--- build.fsx
+++ build.fsx
@@ -1,26 +1,36 @@
 #r "packages/FAKE/tools/FakeLib.dll"
 open System
 open Fake
+open Fake.ProcessHelper
 
 let buildDir = "./build"
+let slnFile = "./Predictions.sln"
 
 Target "Clean" (fun _ ->
   CleanDir buildDir
 )
 
 Target "RestorePackages" (fun _ ->
-  "./Predictions.sln"
+  slnFile
   |> RestoreMSSolutionPackages (fun p -> { p with ToolPath = "./nuget.exe" })
 )
 
 Target "Compile" (fun _ ->
-  !! "./Predictions.sln"
+  !! slnFile
   |> MSBuildRelease buildDir "Build"
   |> ignore
+)
+
+Target "FrontEnd" (fun _ ->
+  let grunt = tryFindFileOnPath (if isUnix then "grunt" else "grunt.cmd")
+  match grunt with
+  | Some g -> Shell.Exec(g, "build") |> ignore
+  | None -> ()
 )
 
 "Clean"
   ==> "RestorePackages"
   ==> "Compile"
+  //==> "FrontEnd"
 
 RunTargetOrDefault "Compile"
