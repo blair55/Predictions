8aebdda9a89c7bb144124389248cd6512d351288
Blair55 <blair.nicholas@googlemail.com>
2016-08-08 00:38:59 +0100

    install fsharp in bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,4 +2,4 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328
 echo "deb http://download.mono-project.com/repo/debian wheezy/snapshots/4.2.4 main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
 echo "deb http://download.mono-project.com/repo/debian wheezy-apache24-compat main" | sudo tee -a /etc/apt/sources.list.d/mono-xamarin.list
 sudo apt-get -qq update
-sudo apt-get -qq install mono-devel ca-certificates-mono
+sudo apt-get -qq install mono-devel ca-certificates-mono fsharp
