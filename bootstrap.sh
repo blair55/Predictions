03fa89d79ff983114e73e4a1276de08cfff5030d
Blair55 <blair.nicholas@googlemail.com>
2016-08-07 22:32:13 +0100

    d

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,4 @@
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
-echo "deb http://download.mono-project.com/repo/debian wheezy-apache24-compat main" | sudo tee -a /etc/apt/sources.list.d/mono-xamarin.list
-#echo "deb http://download.mono-project.com/repo/debian wheezy-libjpeg62-compat main" | sudo tee -a /etc/apt/sources.list.d/mono-xamarin.list
-sudo apt-get update
-sudo apt-get install -y build-essential mono-complete fsharp
-mozroots --import --sync
-#curl -sL https://deb.nodesource.com/setup_0.12 | sudo bash -
-#sudo apt-get install -y nodejs
-#sudo npm install -g grunt-cli
+sudo apt-get -qq update
+sudo apt-get -qq install mono-devel ca-certificates-mono
