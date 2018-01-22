393e5d2bb043538c0e3340677698cd28197a8e01
Blair55 <blair.nicholas@googlemail.com>
2018-01-22 22:39:29 +0000

    Force package restore

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ mono .paket/paket.bootstrapper.exe
 
 # mozroots --import --sync --quiet
 
-mono .paket/paket.exe restore
+mono .paket/paket.exe restore -f
 mono packages/FAKE/tools/FAKE.exe build.fsx compile
